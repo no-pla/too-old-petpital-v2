b959fcbcf1c0216a73e964dd4c4565e674e1229b
no-pla <songjihyun323@gmail.com>
2024-04-04 02:43:31 +0900

    initial commit: 프로젝트 set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
+      colors: {
+        main: "#15B5BF",
+        sub: "#AFE5E9",
       },
     },
   },
