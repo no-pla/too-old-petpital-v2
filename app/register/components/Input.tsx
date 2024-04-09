7cf709ac89d01cead95cb07f4dc42ba7a91685d6
no-pla <songjihyun323@gmail.com>
2024-04-09 21:27:35 +0900

    feat: add input reset value button

--- app/register/components/Input.tsx
+++ app/register/components/Input.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormContext } from "react-hook-form";
+import { IoCloseCircle } from "react-icons/io5";
 
 interface InputData {
   label: string;
@@ -32,19 +33,38 @@ interface ValidationTypes {
 const Input = ({ label, type, validation, id, placeholder }: InputData) => {
   const {
     register,
+    resetField,
+    getValues,
     formState: { errors },
   } = useFormContext();
-  // console.log(errors);
+
+  const resettingField = () => resetField(id);
+
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        {...register(id, validation)}
-      />
-      {errors && errors[id] && <p>{errors[id]?.message?.toString()}</p>}
+      <label htmlFor={id} className="font-bold">
+        {label}
+      </label>
+      <div className="relative my-[8px]">
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          {...register(id, validation)}
+          className="border-[1px] border-[#c5c5c5] py-[18.5px] px-6 font-[16px] w-full focus:border-main outline-none"
+        />
+        {getValues(id) !== "" && (
+          <button
+            onClick={() => resettingField()}
+            className="absolute top-1/2 transform right-4 -translate-y-1/2"
+          >
+            <IoCloseCircle color="#9F9F9F" className="w-6 h-6" />
+          </button>
+        )}
+      </div>
+      {errors && errors[id] && (
+        <p className="text-warn">{errors[id]?.message?.toString()}</p>
+      )}
     </div>
   );
 };
